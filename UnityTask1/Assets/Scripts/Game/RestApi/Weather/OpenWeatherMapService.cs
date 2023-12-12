4756b11c0a5115ece0b2b4275233c8815f24906c
FoxSuda <kote0085@gmail.com>
2023-12-13 01:23:15 +0200

    Fix class name

--- UnityTask1/Assets/Scripts/Game/RestApi/Weather/OpenWeatherMapService.cs
+++ UnityTask1/Assets/Scripts/Game/RestApi/Weather/OpenWeatherMapService.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour, IWeatherService
+public class OpenWeatherMapService : MonoBehaviour, IWeatherService
 {
     private string apiKey = "d9edbb1b6ff1e03612e4b673539b8cbb";
     private string weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid={2}";
